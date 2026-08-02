471d87a6a6690bb87b1a1ad2c532132344945e4a
Golu <140903720+GoluScriptMage@users.noreply.github.com>
2026-08-02 18:31:04 +0530

    feat: added http-proxy in the proxy server for deployed container

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -9,31 +9,59 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
 import fs from "node:fs"
+import { DatabaseSync } from 'node:sqlite';
+import { createProxyServer } from 'http-proxy';
 
 const app = express();
+const proxy = createProxyServer();
+
+// 1. Database path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dbPath = path.resolve(__dirname, "../../server/aerocloud.db");
+
+// 2. Database connection
+const db = new DatabaseSync(dbPath);
+
+// 3. Query the db for the subdomain and get the deployment path
+function getDeploymentPath(subdomain: string) {
+    const statement = db.prepare("SELECT * FROM deployments WHERE subdomain = ? AND status = 'deployed'");
+    return statement.get(subdomain) as { subdomain: string, port: number, status: string } | null;
+}
 
 app.use((req, res, next) => {
+
+
     const host = req.get('host');
 
     if (!host) res.status(404).send("Deployment not found");
     const subDomain = host?.split('.')[0];
 
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
+    const deployment = getDeploymentPath(subDomain!);
 
-    const deploymentsDir = path.resolve(__dirname, "../../server/deployments"); // This drop 2 level to parent and search for sevrer folder
-    const finalPath = path.join(deploymentsDir, subDomain!);
+    // If deployments doesn't exist go for static server
+    if (!deployment) {
+        const deploymentsDir = path.resolve(__dirname, "../../server/de/ployments"); // This drop 2 level to parent and search for sevrer folder
+        const finalPath = path.join(deploymentsDir, subDomain!);
 
-    // Check for final path
-    if (fs.existsSync(finalPath)) {
-        console.log("Came here")
-        // Execute dynamic static server
-        express.static(finalPath)(req, res, next);
-    } else {
-        res.status(404).send("Deployment not found");
+        // Check for final path
+        if (fs.existsSync(finalPath)) {
+            // Execute dynamic static server
+            express.static(finalPath)(req, res, next);
+            return;
+        } else {
+            res.status(404).send("Deployment not found");
+        }
     }
 
-    console.log("Final Path logging: ", finalPath);
+    if (deployment!.status === "deployed" && deployment?.port) {
+        // Level 2: Prxoy to docker container
+        proxy.web(req, res, {
+            target: `http://localhost:${deployment.port}`
+        })
+    }
+
+
 })
 
 app.listen(8080, () => {
