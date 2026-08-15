0e0a593596ceb8e87cc0e8032f71b6bb0747c581
Golu <140903720+GoluScriptMage@users.noreply.github.com>
2026-08-15 08:24:34 +0530

    feat(db): added method to create api key & convert to hash

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,17 +1,20 @@
 import Database from "better-sqlite3";
 import type { Database as DatabaseType } from "better-sqlite3";
+import crypto from "crypto";
 
 // Initialize the database connection
 const db: DatabaseType = new Database('aerocloud.db');
+
+// Email null allowed to handle private profiles
 
 db.exec(`
     CREATE TABLE IF NOT EXISTS users (
         githubId TEXT PRIMARY KEY,
         username TEXT NOT NULL,
-        email TEXT NOT NULL,
+        email TEXT NULL, 
         apiKeyHash TEXT UNIQUE NULL,
         createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
-    )
+    );
     
     CREATE TABLE IF NOT EXISTS deployments (
         subdomain TEXT PRIMARY KEY,
@@ -21,10 +24,22 @@ db.exec(`
         createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
         envVars TEXT NULL,
         userId INTEGER NULL,
-        FOREIGN KEY(userId) REFRENCES users(githubId)
+        FOREIGN KEY(userId) REFERENCES users(githubId)
         
     )
 `)
+
+// Function to save or update user 
+export function saveUser(githubId: string, username: string, email: string | null, apiKeyHash: string | null) {
+    const statement = db.prepare('INSERT INTO users(githubId, username, email, apiKeyHash) VALUES (?, ?, ?, ?)');
+    statement.run(githubId, username, email, apiKeyHash);
+}
+
+// Function to fetch user by apiKey 
+export function getUserByApiKey(apiKeyHash: string) {
+    const statement = db.prepare('SELECT * FROM users WHERE apiKeyHash = ?');
+    return statement.get(apiKeyHash);
+}
 
 // Function to save deployment information to the database
 export function saveDeployment(subdomain: string, port: number, status: string, envVars?: string, userId?: string) {
@@ -62,5 +77,14 @@ export function getUsedPorts() {
     return statement.all();
 }
 
+// Helper function to hash the raw api key
+export function hashApiKey(apiKey: string): string {
+    return crypto.createHash('sha256').update(apiKey).digest('hex'); // Converts the hash to a hexadecimal string
+}
+
+// Function to generate a unique API key for a user
+export function generateApiKey(): string {
+    return 'ac_live' + crypto.randomBytes(24).toString('hex'); // Generates a 48-character hexadecimal string
+}
 
 export default db;
