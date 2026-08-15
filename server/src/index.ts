e3ced1df73a2b1a4214a7e08a19e0d0309d885a5
Golu <140903720+GoluScriptMage@users.noreply.github.com>
2026-08-15 09:44:13 +0530

    feat(routes): implemented security layers on public &secured routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,28 +13,29 @@ import { destroyContainer, stopContainer } from "./routes/stopContainer.js";
 import { getCrashLogs } from "./routes/logs.js";
 import { authRoutes } from "./routes/auth.js";
 import dotenv from "dotenv";
+import { authenticateUserMiddleware } from "./utils/middleware.js";
 
-dotenv.config(); 
+dotenv.config();
 const app = express();
 
-// Deploy routes 
-deployRoutes(app);
+// Middlewares to parse JSON and handle file uploads
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// List deployment route
-listDeployments(app);
-
-// Stop container route
-stopContainer(app);
-
-// Destroy container route
-destroyContainer(app);
-
-// Logs the crashed or deployed container logs 
-getCrashLogs(app);
-
-// Auth routes
+// Public routes (no authentication required)
 authRoutes(app);
 
+// Global Auth Guard (  applies to all routes below this line) 
+app.use(authenticateUserMiddleware);
+
+// Protected routes (authentication required)
+deployRoutes(app); // Handles deployment of applications
+listDeployments(app); // Lists all deployments for the authenticated user
+stopContainer(app); // Stops a specific container for the authenticated user
+destroyContainer(app); // Destroys a specific container for the authenticated user
+getCrashLogs(app); // Retrieves crash logs for a specific container for the authenticated user
+
+// Start the server
 app.listen(3000, () => {
     Logger.info("Server running on port 3000");
 })
